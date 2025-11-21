fcfb981ad978ee1467db42edaa337b05bd177d7e
coding-rabbit-AI <sungbings@naver.com>
2025-11-22 00:17:16 +0900

    update

--- sungb_workspace/CNN_Model/check.py
+++ sungb_workspace/CNN_Model/check.py
@@ -1,7 +1,7 @@
 import torch
 
 
-state = torch.load("./train/exp1/best_model.pth", map_location='cuda:0') 
+state = torch.load("./train/exp2/best_model.pth", map_location='cuda:0') 
 for k, v in state.items():
     print(f"{k}: {v.shape}")
 
